8a7ee507b8a5081bcdaddd27f709909fc9fb9cd3
Zafirmk <zafirmk0@gmail.com>
2024-03-22 16:48:22 +0400

    add multihead attention class

--- model.py
+++ model.py
@@ -1,9 +1,10 @@
 import torch
 import torch.nn as nn
+import numpy as np
 torch.manual_seed(1)
 
 class WordEmbeddings(nn.Module):
-    def __init__(self, d_model, vocab_size, *args, **kwargs) -> None:
+    def __init__(self, d_model: int, vocab_size: int, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.d_model = d_model
         self.vocab_size = vocab_size
@@ -16,7 +17,7 @@ class WordEmbeddings(nn.Module):
         return self.embedding_table(x) * torch.sqrt(torch.tensor(self.d_model))
 
 class PositionalEmbeddings(nn.Module):
-    def __init__(self, d_model, seq_len, *args, **kwargs) -> None:
+    def __init__(self, d_model: int, seq_len: int, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.d_model = d_model
         self.seq_len = seq_len
@@ -36,7 +37,7 @@ class PositionalEmbeddings(nn.Module):
         with torch.no_grad():
             return x + self.p_embed_table[:, :x.shape[1], :]
 class FeedForward(nn.Module):
-    def __init__(self, d_model, d_hidden, *args, **kwargs) -> None:
+    def __init__(self, d_model: int, d_hidden: int, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.d_model = d_model
         self.d_hidden = d_hidden
@@ -52,7 +53,7 @@ class FeedForward(nn.Module):
         return x
 
 class ResidualConnection(nn.Module):
-    def __init__(self, d_model, *args, **kwargs) -> None:
+    def __init__(self, d_model: int, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.layer_norm = torch.nn.LayerNorm(d_model)
     
@@ -61,20 +62,60 @@ class ResidualConnection(nn.Module):
         return self.layer_norm(torch.add(f, x))
 
 class MultiHeadAttention(nn.Module):
-    pass
+    def __init__(self, d_model: int, num_heads: int, mask: torch.Tensor = None, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        
+        assert d_model % num_heads == 0, "d_model must be divisible by num_heads"
+
+        self.d_k = d_model / num_heads
+        self.softmax = torch.nn.Softmax(dim=-1)
+        self.d_model = d_model
+        self.num_heads = num_heads
+        self.mask = mask
+
+        self.w_q = torch.randn((self.d_model, self.d_model))
+        self.w_k = torch.randn((self.d_model, self.d_model))
+        self.w_v = torch.randn((self.d_model, self.d_model))
+        self.w_o = torch.randn((self.d_model, self.d_model))
+    
+    def attention(self, q: torch.Tensor, k: torch.Tensor, v: torch.Tensor):
+        numerator = q @ k.transpose(-2, -1)
+        denominator = np.sqrt(self.d_k)
+        attention = self.softmax(numerator/denominator)
+        if self.mask:
+            attention = attention @ self.mask
+        return attention @ v
+
+    def forward(self, q: torch.Tensor, k: torch.Tensor, v: torch.Tensor):
+        q_ = q @ self.w_q
+        k_ = k @ self.w_k
+        v_ = v @ self.w_v
+
+        # (batch, seq, d_model) ---> (batch, num_heads, seq, d_k)
+        q_ = q_.view(q_.shape[0], self.num_heads, q.shape[1], int(self.d_k))
+        k_ = k_.view(k_.shape[0], self.num_heads, k.shape[1], int(self.d_k))
+        v_ = v_.view(v_.shape[0], self.num_heads, v.shape[1], int(self.d_k))
+
+        attention_heads = self.attention(q_, k_, v_)
+
+        # (batch, num_heads, seq, d_k) ---> (batch, seq, d_model)
+        h = attention_heads.view(attention_heads.shape[0], attention_heads.shape[2], attention_heads.shape[1]*attention_heads.shape[3])
+
+        return h @ self.w_o
 
 
-# we = WordEmbeddings(4, 24)
-# pe = PositionalEmbeddings(4, 10)
+# we = WordEmbeddings(512, 24)
+# pe = PositionalEmbeddings(512, 10)
 # ff = FeedForward(4, 20)
 # rs = ResidualConnection(4)
+# ma = MultiHeadAttention(512, 8)
 
 # s = "I wonder what will come next"
 # tokens = torch.LongTensor([[11, 23, 21, 22, 5, 15]])
 
 # word_embed = we(tokens)
 # pos_embed = pe(word_embed)
-# post_rs = rs(pos_embed, ff)
+# post_ma = ma(pos_embed, pos_embed, pos_embed)
 
 
 # print(post_rs)

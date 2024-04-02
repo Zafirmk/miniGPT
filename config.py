62f8f43fb345b540f9dfe3a39971337da20198d3
Zafirmk <zafirmk0@gmail.com>
2024-04-02 04:24:01 +0400

    Update config.py with new vocabulary sizes and epochs

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 config = {
     "d_model": 512,
-    "vocab_size": 141,
+    "enc_vocab_size": 141,
+    "dec_vocab_size": 150,
     "max_seq_len": 350,
     "d_hidden": 2048,
     "num_heads": 8,
-    "num_blocks": 8
+    "num_blocks": 8,
+    "epochs": 10
 }
 
 def get_config() -> dict:

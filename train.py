26b6e80c0b44ef1287ead53667f24a489e5f7676
Zafirmk <zafirmk0@gmail.com>
2024-04-22 01:27:27 +0400

    change to single machine training

--- train.py
+++ train.py
@@ -1,11 +1,13 @@
 import os
 import numpy as np
+from tqdm import tqdm
 from config import get_config
 from model import EncoderDecoderTransformer
 from utils.log import collect_stats, init_stats
 from torch.utils.data import DataLoader
 import torch
 from torch.nn.parallel import DistributedDataParallel
+from utils.model_utils import run_validation
 
 class Trainer:
     def __init__(self,
@@ -13,34 +15,37 @@ class Trainer:
                  optimizer: torch.optim.Adam,
                  loss_fn: torch.nn.CrossEntropyLoss,
                  train_dataloader: DataLoader,
+                 val_dataloader: DataLoader,
                  snapshot_pth: str = 'latest_snapshot.pt',
                  epochs_run: int = 0
         ) -> None:
-        self.local_rank = int(os.environ['LOCAL_RANK'])
-        self.global_rank = int(os.environ['RANK'])
-        self.model = model.to(self.local_rank)
+        # self.local_rank = int(os.environ['LOCAL_RANK'])
+        # self.global_rank = int(os.environ['RANK'])
+        self.model = model
         self.optimizer = optimizer
-        self.loss_fn = loss_fn.to(self.local_rank)
+        self.loss_fn = loss_fn
         self.train_dataloader = train_dataloader
+        self.val_dataloader = val_dataloader
         self.snapshot_pth = snapshot_pth
         self.epochs_run = epochs_run
         self.config = get_config()
+        self.causal_mask = torch.nn.Transformer.generate_square_subsequent_mask(self.config['max_seq_len'])
         if os.path.exists(self.snapshot_pth):
-            print(f"Loading Snapshot for GPU:{self.global_rank}")
+            # print(f"Loading Snapshot for GPU:{self.global_rank}")
             self.load_snapshot()
-        self.model = DistributedDataParallel(self.model, device_ids=[self.local_rank], find_unused_parameters=True)
+        # self.model = DistributedDataParallel(self.model, device_ids=[self.local_rank], find_unused_parameters=True)
 
     def process_batch(self, batch):
         self.optimizer.zero_grad()
-        enc_tokens = batch['enc_tokens'].to(self.local_rank)
-        dec_tokens = batch['dec_tokens'].to(self.local_rank)
+        enc_tokens = batch['enc_tokens']
+        dec_tokens = batch['dec_tokens']
 
-        enc_mask = batch['enc_mask'].to(self.local_rank)
-        dec_mask = batch['dec_mask'].to(self.local_rank)
+        enc_mask = batch['enc_padding_mask']
+        dec_mask = batch['dec_padding_mask']
 
-        label = batch['label'].to(self.local_rank)
+        label = batch['label']
 
-        pred = self.model(enc_tokens, dec_tokens, enc_mask, dec_mask).to(self.local_rank)
+        pred = self.model(enc_tokens, dec_tokens, enc_mask, dec_mask, self.causal_mask)
         loss = self.loss_fn(pred.view(-1, self.config['dec_vocab_size']), label.view(-1))
 
         loss.backward()
@@ -48,14 +53,15 @@ class Trainer:
         return loss
 
     def process_epoch(self, epoch):
-        self.train_dataloader.sampler.set_epoch(epoch)
+        # self.train_dataloader.sampler.set_epoch(epoch)
         b_sz = (next(iter(self.train_dataloader)))['enc_tokens'].shape[0]
         total_loss = 0
-        print(f"Start: [GPU:{self.global_rank}] | Epoch: {epoch} | BSize: {b_sz} | Steps: {len(self.train_dataloader)}")
+        print(f"Start: Epoch: {epoch} | BSize: {b_sz} | Steps: {len(self.train_dataloader)}")
         for idx, batch in enumerate(self.train_dataloader):
             batch_loss = self.process_batch(batch)
             total_loss += batch_loss
-        print(f"Complete [GPU:{self.global_rank}] | Epoch: {epoch} | Loss: {total_loss / len(batch)}")
+            break
+        print(f"Complete Epoch: {epoch} | Loss: {total_loss / len(batch)}")
         return total_loss / len(batch)
 
     def save_snapshot(self, epoch):
@@ -74,11 +80,12 @@ class Trainer:
         print(f"Resuming training from snapshot at Epoch: {self.epochs_run}")
 
     def train(self):
-        if self.local_rank == 0:
-            init_stats()
-        for epoch in range(self.epochs_run, self.config["epochs"]):
+        # if self.local_rank == 0:
+            # init_stats()
+        for epoch in tqdm(range(self.epochs_run, self.config["epochs"])):
             epoch_loss = self.process_epoch(epoch)
-            if self.local_rank == 0:
-                collect_stats(epoch_loss, 0, epoch)
-                if epoch % 5 == 0:
-                    self.save_snapshot(epoch)
+            run_validation(model=self.model, validation_ds=self.val_dataloader, max_len=self.config['max_seq_len'])
+            # if self.local_rank == 0:
+                # collect_stats(epoch_loss, 0, epoch)
+                # if epoch % 5 == 0:
+                    # self.save_snapshot(epoch)
